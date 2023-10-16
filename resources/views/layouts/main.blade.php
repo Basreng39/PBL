068a643bc4d26ce3272c60be652293fad910ed12
Hasan <Hasan@mail.com>
2023-10-16 10:17:16 +0700

    add login

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>KOSCantik Kediri</title>
+
+    {{-- Boostrap css --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    {{-- My css Style --}}
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <div class="containe">
+        @yield('content')
+    </div>
+</body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="row justify-content-center">
+    <div class="col-md-4">
+        <main class="form-signin w-100 m-auto">
+            <form>
+                <center>
+                    <img class="mb-4 mt-5" src="/img/logo.jpg" alt="" width="72" height="57">
+                </center>
+                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+                <div class="form-floating">
+                    <input type="text" class="form-control" id="floatingInput" placeholder="Username">
+                    <label for="floatingInput">Username</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Password</label>
+                </div>
+
+                <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+            </form>
+        </main>
+    </div>
+</div>
+
+
+
+@endsection
